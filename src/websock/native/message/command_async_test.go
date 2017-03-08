ca698bea87dd40d0d554d0d2d5077fa42322fd0f
James Harris <james.harris@icecave.com.au>
2017-03-08 14:23:42 +1000

    Add async call response message types.

--- src/websock/native/message/command_async_test.go
+++ src/websock/native/message/command_async_test.go
@@ -53,3 +53,92 @@ var _ = Describe("AsyncCall", func() {
 		})
 	})
 })
+
+var _ = Describe("AsyncSuccess", func() {
+	Describe("write", func() {
+		It("encodes the message", func() {
+			var buf bytes.Buffer
+			p := rinq.NewPayload("payload")
+			m := &AsyncSuccess{
+				Session: 0xabcd,
+				Header: AsyncSuccessHeader{
+					Namespace: "ns",
+					Command:   "cmd",
+				},
+				Payload: p,
+			}
+
+			err := Write(&buf, JSONEncoding, m)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			expected := []byte{
+				'A', 'S',
+				0xab, 0xcd, // session index
+				0, 12, // header size
+			}
+			expected = append(expected, `["ns","cmd"]`...)
+			expected = append(expected, `"payload"`...)
+			Expect(buf.Bytes()).To(Equal(expected))
+		})
+	})
+})
+
+var _ = Describe("AsyncFailure", func() {
+	Describe("write", func() {
+		It("encodes the message", func() {
+			var buf bytes.Buffer
+			p := rinq.NewPayload("payload")
+			m := &AsyncFailure{
+				Session: 0xabcd,
+				Header: AsyncFailureHeader{
+					Namespace:      "ns",
+					Command:        "cmd",
+					FailureType:    "fail-type",
+					FailureMessage: "message",
+				},
+				Payload: p,
+			}
+
+			err := Write(&buf, JSONEncoding, m)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			expected := []byte{
+				'A', 'F',
+				0xab, 0xcd, // session index
+				0, 34, // header size
+			}
+			expected = append(expected, `["ns","cmd","fail-type","message"]`...)
+			expected = append(expected, `"payload"`...)
+			Expect(buf.Bytes()).To(Equal(expected))
+		})
+	})
+})
+
+var _ = Describe("AsyncError", func() {
+	Describe("write", func() {
+		It("encodes the message", func() {
+			var buf bytes.Buffer
+			m := &AsyncError{
+				Session: 0xabcd,
+				Header: AsyncErrorHeader{
+					Namespace: "ns",
+					Command:   "cmd",
+				},
+			}
+
+			err := Write(&buf, JSONEncoding, m)
+
+			Expect(err).ShouldNot(HaveOccurred())
+
+			expected := []byte{
+				'A', 'E',
+				0xab, 0xcd, // session index
+				0, 12, // header size
+			}
+			expected = append(expected, `["ns","cmd"]`...)
+			Expect(buf.Bytes()).To(Equal(expected))
+		})
+	})
+})
