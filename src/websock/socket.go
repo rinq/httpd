2d35d9cf88f69a1f04277028ba6f562b5da80846
James Harris <james.harris@icecave.com.au>
2017-03-13 10:08:49 +1000

    Narrow the `Socket` interface.

--- src/websock/socket.go
+++ src/websock/socket.go
@@ -7,13 +7,9 @@ import (
 
 // Socket is an interface for Gorilla's websocket.Conn struct.
 type Socket interface {
-	Subprotocol() string
-
 	SetReadDeadline(t time.Time) error
 	SetPongHandler(h func(string) error)
-
 	NextReader() (int, io.Reader, error)
 	NextWriter(int) (io.WriteCloser, error)
-
 	WriteMessage(mt int, data []byte) error
 }
